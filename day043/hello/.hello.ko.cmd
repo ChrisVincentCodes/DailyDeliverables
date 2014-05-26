980918aa100dd403328c0666c71bc8084d3d5985
ChrisVincentCodes <chrisvincentcodes@gmail.com>
2014-05-26 14:24:13 -0600

    linux kernel module

---
+++ day043/hello/.hello.ko.cmd
@@ -0,0 +1,1 @@
+cmd_/home/fiveguys/Desktop/hello/hello.ko := ld -r -m elf_i386 -T /usr/src/linux-headers-3.13.0-24-generic/scripts/module-common.lds --build-id  -o /home/fiveguys/Desktop/hello/hello.ko /home/fiveguys/Desktop/hello/hello.o /home/fiveguys/Desktop/hello/hello.mod.o
---
+++ day043/hello/.tmp_versions/hello.mod
@@ -0,0 +1,2 @@
+/home/fiveguys/Desktop/hello/hello.ko
+/home/fiveguys/Desktop/hello/hello.o
